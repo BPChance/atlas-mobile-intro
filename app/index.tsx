5b58f7af950c13cfed365187a4fe002e274b9dd6
BPChance <bradenpchance6@gmail.com>
2025-06-18 16:31:32 -0500

    added delete button below add activity button, implemented swipe gesture for deleting single items, updated styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,13 @@
 import { router } from "expo-router";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { useEffect, useState } from "react";
-import { fetchActivities } from "@/utils/database";
+import {
+  fetchActivities,
+  deleteAllActivities,
+  deleteActivityById,
+} from "@/utils/database";
 import { FlashList } from "@shopify/flash-list";
+import { Swipeable } from "react-native-gesture-handler";
 
 type Activity = {
   id: number;
@@ -28,21 +33,39 @@ export default function Index() {
     return date.toLocaleString();
   }
 
+  function renderActions(id: number) {
+    return (
+      <Pressable
+        onPress={async () => {
+          await deleteActivityById(id);
+          loadActivities();
+        }}
+        style={styles.swipeDelete}
+      >
+        <Text style={styles.deleteButtonText}>Delete</Text>
+      </Pressable>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Home Screen</Text>
       <View style={styles.listContainer}>
         <FlashList
           data={activities}
           keyExtractor={(item) => item.id.toString()}
           estimatedItemSize={50}
           renderItem={({ item }) => (
-            <View style={styles.card}>
-              <Text style={styles.cardDate}>{formatDate(item.date)}</Text>
-              <Text style={styles.cardSteps}>
-                Steps: {item.steps.toLocaleString()}
-              </Text>
-            </View>
+            <Swipeable
+              renderRightActions={() => renderActions(item.id)}
+              renderLeftActions={() => renderActions(item.id)}
+            >
+              <View style={styles.card}>
+                <Text style={styles.cardDate}>{formatDate(item.date)}</Text>
+                <Text style={styles.cardSteps}>
+                  Steps: {item.steps.toLocaleString()}
+                </Text>
+              </View>
+            </Swipeable>
           )}
           ListEmptyComponent={<Text>No entries yet</Text>}
         />
@@ -55,6 +78,15 @@ export default function Index() {
       >
         <Text style={styles.buttonText}>Add Activity</Text>
       </Pressable>
+      <Pressable
+        style={[styles.button, styles.deleteButton]}
+        onPress={async () => {
+          await deleteAllActivities();
+          loadActivities();
+        }}
+      >
+        <Text style={styles.buttonText}>Delete all activities</Text>
+      </Pressable>
     </View>
   );
 }
@@ -65,24 +97,27 @@ const styles = StyleSheet.create({
     paddingTop: 60,
     backgroundColor: "#FEF9E6",
   },
-  heading: {
-    fontSize: 24,
-    marginBottom: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  item: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
   button: {
-    padding: 15,
+    padding: 20,
     backgroundColor: "#1ED2AF",
     alignItems: "center",
   },
   buttonText: {
     color: "white",
-    fontWeight: "bold",
+  },
+  deleteButton: {
+    backgroundColor: "#D00414",
+  },
+  deleteButtonText: {
+    color: "white",
+  },
+  swipeDelete: {
+    backgroundColor: "#D00414",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    marginBottom: 12,
+    height: 70,
   },
   listContainer: {
     flex: 1,
@@ -96,12 +131,12 @@ const styles = StyleSheet.create({
     marginBottom: 12,
   },
   cardDate: {
-    fontSize: 12,
+    fontSize: 14,
     color: "black",
     marginBottom: 2,
   },
   cardSteps: {
-    fontSize: 24,
+    fontSize: 26,
     marginBottom: 8,
   },
 });
